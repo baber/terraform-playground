649698c9823ebc0a114fb98c503aaa640d02ad40
Baber Khalil <Baber.s.Khalil@googlemail.com>
2017-10-19 15:33:51 +0100

    Using backticks to run terraform.

--- spec/ec2/create_ec2_spec.rb
+++ spec/ec2/create_ec2_spec.rb
@@ -4,10 +4,12 @@ require 'ec2/spec_helper'
 describe ec2("#{@instance_name}"), :integration do
 
   before(:all) do
+    ENV['AWS_PROFILE'] = 'terraform'
+    ENV['AWS_REGION'] = 'eu-west-2'
 
   command = Thread.new do
-    system("terraform","init", "-var", "instance_name=#{@instance_name}", "-var", "security_group=#{RSpec.configuration.security_group}", "configurations")
-    system("terraform", "apply", "-var", "instance_name=#{@instance_name}", "-var", "security_group=#{RSpec.configuration.security_group}", "configurations")
+    `terraform init -var instance_name=#{@instance_name} -var security_group=#{RSpec.configuration.security_group} /Users/baberkhalil/software/projects/EqualExperts/HMRC/workspace/terraform-playground/configurations`
+    `terraform apply -var instance_name=#{@instance_name} -var security_group=#{RSpec.configuration.security_group} /Users/baberkhalil/software/projects/EqualExperts/HMRC/workspace/terraform-playground/configurations`
   end
   command.join
 
@@ -22,10 +24,7 @@ describe ec2("#{@instance_name}"), :integration do
   it { should have_security_groups(["#{RSpec.configuration.security_group}"]) }
 
   after(:all) do
-    command = Thread.new do
-      system("terraform", "destroy", "-var", "instance_name=#{@instance_name}", "-var", "security_group=#{RSpec.configuration.security_group}", "-force", "configurations")
-    end
-    command.join
+    `terraform destroy -force -var instance_name=#{@instance_name} -var security_group=#{RSpec.configuration.security_group} /Users/baberkhalil/software/projects/EqualExperts/HMRC/workspace/terraform-playground/configurations`
   end
 
 end
--- spec/security-groups/security-groups_spec.rb
+++ spec/security-groups/security-groups_spec.rb
@@ -5,12 +5,11 @@ describe security_group("allow_whitelisted_ssh"), :unit do
 
   before(:all) do
 
-  command = Thread.new do
-    system("terraform","init", "configurations/security-groups")
-    system("terraform", "apply", "configurations/security-groups")
-  end
-  command.join
+    ENV['AWS_PROFILE'] = 'terraform'
+    ENV['AWS_REGION'] = 'eu-west-2'
 
+    `terraform init /Users/baberkhalil/software/projects/EqualExperts/HMRC/workspace/terraform-playground/configurations/security-groups`
+    `terraform apply /Users/baberkhalil/software/projects/EqualExperts/HMRC/workspace/terraform-playground/configurations/security-groups`
   end
 
 
@@ -20,10 +19,8 @@ describe security_group("allow_whitelisted_ssh"), :unit do
   its(:inbound) { should be_opened(22).protocol('tcp') }
 
   after(:all) do
-    command = Thread.new do
-      system("terraform", "destroy", "-force", "configurations/security-groups")
-    end
-    command.join
+
+    `terraform destroy -force /Users/baberkhalil/software/projects/EqualExperts/HMRC/workspace/terraform-playground/configurations/security-groups`
   end
 
 end
